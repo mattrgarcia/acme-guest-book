3e651489492e2ccacaf642bf7dcbe15bb61db5d0
Matt Garcia <mattrg1105@gmail.com>
2019-09-09 19:58:28 -0700

    displays guests from json file

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res)=> {
-  res.write('hello world');
-  res.end();
+const readFile = (file)=> {
+  return new Promise((resolve, reject)=> {
+    fs.readFile(file, (err, data)=> {
+      if(err){
+        reject(err)
+      }
+      else {
+        resolve(data.toString());
+      }
+    });
+  });
+};
+
+const readFileJSON = async (file)=> {
+  const jsonString = await readFile(file);
+  return JSON.parse(jsonString);
+}
+const server = http.createServer( async (req, res)=> {
+  if(req.url === '/api/guests'){
+    const guests = await readFileJSON('./guests.json');
+    res.write(JSON.stringify(guests));
+    res.end();
+  }
 });
 
 server.listen(3000);
